26344e2f72cde15a23d73757987f3e30b3b119e3
lostfile1 <41842361+lostfile1@users.noreply.github.com>
2019-10-13 10:51:44 -0400

    added ablity to set out put txt file name

--- cimage.py
+++ cimage.py
@@ -10,7 +10,11 @@ with open(filename, "rb") as img_file:
     my_string = base64.b64encode(img_file.read())
 
 print(my_string.decode('utf-8'))
+print("encoding finished")
 
-f= open("guru99.txt","w+")
+outtput = raw_input('output textfile name: ')
+f= open(outtput,"w+")
 f.write(my_string.decode('utf-8'))
 f.close()
+
+print("writing to file finished")
